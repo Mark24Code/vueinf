6316d366b28eeeb0c39822529e8840bd3c058438
mark24 <mark.zhangyoung@qq.com>
2017-01-06 01:38:22 +0800

    demo

---
+++ src/components/Hello.vue
@@ -0,0 +1,68 @@
+<template>
+    <ul id="demo">
+        <item class="item" :model="treeData">
+        </item>
+    </ul>
+</template>
+<script>
+import item from './item.vue'
+export default {
+    name: 'hello',
+    components:{
+      item
+    },
+    data() {
+        return {
+            treeData: {
+                name: 'My Tree',
+                children: [{
+                    name: 'hello'
+                }, {
+                    name: 'wat'
+                }, {
+                    name: 'child folder',
+                    children: [{
+                        name: 'child folder',
+                        children: [{
+                            name: 'hello'
+                        }, {
+                            name: 'wat'
+                        }]
+                    }, {
+                        name: 'hello'
+                    }, {
+                        name: 'wat'
+                    }, {
+                        name: 'child folder',
+                        children: [{
+                            name: 'hello'
+                        }, {
+                            name: 'wat'
+                        }]
+                    }]
+                }]
+            }
+        }
+    }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.lfbox {
+    width: 100px;
+    height: 200px;
+    float: left;
+    border: 1px solid #000;
+    box-sizing: border-box;
+    text-align: center;
+}
+
+.itembox {
+    width: 95px;
+    height: 50px;
+    border: 1px solid #999;
+    text-align: center;
+    clear: both;
+    margin: 2px auto;
+}
+</style>
---
+++ src/components/item.vue
@@ -0,0 +1,54 @@
+<template>
+    <li>
+        <div class="lfbox" :class="{bold: isFolder}" @click="toggle" @dblclick="changeType">
+            {{model.name}}
+            <span v-if="isFolder">[{{open ? '-' : '+'}}]</span>
+        </div>
+        <ul v-show="open" v-if="isFolder">
+            <item class="item itembox" v-for="model in model.children" :model="model">
+            </item>
+            <li class="add" @click="addChild">+</li>
+        </ul>
+    </li>
+</template>
+<script>
+import Vue from 'vue'
+export default {
+    name: 'item',
+    props: {
+        model: Object
+    },
+    data() {
+        return {
+            open: false
+        }
+    },
+    computed: {
+        isFolder: function() {
+            return this.model.children &&
+                this.model.children.length
+        }
+    },
+    methods: {
+        toggle: function() {
+            if (this.isFolder) {
+                this.open = !this.open
+            }
+        },
+        changeType: function() {
+            if (!this.isFolder) {
+                Vue.set(this.model, 'children', [])
+                this.addChild()
+                this.open = true
+            }
+        },
+        addChild: function() {
+            this.model.children.push({
+                name: 'new stuff'
+            })
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
